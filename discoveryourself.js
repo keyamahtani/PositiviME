b8d1a0463d561d5ac8cac00955db6dcc8378263f
keyamahtani <41436723+keyamahtani@users.noreply.github.com>
2018-08-08 13:25:38 -0400

    Add files via upload

---
+++ discoveryourself.js
@@ -0,0 +1,137 @@
+var myQuestions = [
+	{
+		question: "When it comes to cleaning...",
+		answers: {
+			a: 'I always want to clean when I see things out of place.',
+			b: 'I only clean when I absolutely need to.',
+
+		},
+		correctAnswer: 'b'
+	},
+	{
+		question: "Do you prefer to stick to tested methods or create your own?",
+		answers: {
+			a: 'Tested methods!',
+			b: 'Create my own!',
+		},
+		correctAnswer: 'b'
+	},
+  {
+		question: "In group projects, I find myself...",
+		answers: {
+			a: 'Delegating tasks to keep everyone organized.',
+			b: 'Focusing on my own tasks.',
+		},
+		correctAnswer: 'b'
+	},
+  {
+    question: "On vacation...",
+    answers: {
+      a: 'I follow an itinerary that I created in advance.',
+      b: 'explore and take it as it goes!',
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "I would prefer to be a...",
+    answers: {
+      a: 'Fashion Stylist.',
+      b: 'Makeup artist.',
+    },
+    correctAnswer: 'b'
+  }
+];
+
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+	function showQuestions(questions, quizContainer){
+		// we'll need a place to store the output and the answer choices
+		var output = [];
+		var answers;
+
+		// for each question...
+		for(var i=0; i<questions.length; i++){
+
+			// first reset the list of answers
+			answers = [];
+
+			// for each available answer...
+			for(letter in questions[i].answers){
+
+				// ...add an html radio button
+				answers.push(
+					'<label>'
+						+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
+						+ letter + ': '
+						+ questions[i].answers[letter]
+					+ '</label>'
+				);
+			}
+
+			// add this question and its answers to the output
+			output.push(
+				'<div class="question">' + questions[i].question + '</div>'
+				+ '<div class="answers">' + answers.join('') + '</div>'
+			);
+		}
+
+		// finally combine our output list into one string of html and put it on the page
+		quizContainer.innerHTML = output.join('');
+	}
+
+
+	function showResults(questions, quizContainer, resultsContainer){
+
+		// gather answer containers from our quiz
+		var answerContainers = quizContainer.querySelectorAll('.answers');
+
+		// keep track of user's answers
+		var userAnswer = '';
+		var numCorrect = 0;
+
+		// for each question...
+		for(var i=0; i<questions.length; i++){
+
+			// find selected answer
+			userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+
+			// if answer is correct
+			if(userAnswer===questions[i].correctAnswer){
+				// add to the number of correct answers
+				numCorrect++;
+
+				// color the answers green
+				answerContainers[i].style.color = 'pink';
+			}
+			// if answer is wrong or blank
+			else{
+				// color the answers red
+				answerContainers[i].style.color = 'pink';
+			}
+		}
+    if(numCorrect>=3){
+      alert("You are right brain dominant! Scroll down to find out more!");
+    }
+    else{
+      alert("You are left brain dominant! Scroll down to find out more!")
+    }
+
+		// show number of correct answers out of total
+		//resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+	}
+
+	// show questions right away
+	showQuestions(questions, quizContainer);
+
+	// on submit, show results
+	submitButton.onclick = function(){
+		showResults(questions, quizContainer, resultsContainer);
+	}
+
+}
