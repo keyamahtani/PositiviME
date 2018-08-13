1db6c2274d2186a8fa8f8968ed510e2a5dbe7fb2
keyamahtani <41436723+keyamahtani@users.noreply.github.com>
2018-08-13 12:20:19 -0400

    Add files via upload

---
+++ buttons.js
@@ -0,0 +1,19 @@
+function eye() {
+  alert("The sky is an infinite movie to me. I never get tired of looking at what is happening up there. - K.D. Leng" );
+}
+
+function heart() {
+  alert("Today you are you! That is truer than true! There is no one alive who is you-er than you! -Dr. Seuss" );
+}
+
+function brain() {
+  alert("The more that you read, the more things you will know. The more that you learn, the more places you'll go. - Dr. Seuss");
+}
+
+function mouth() {
+  alert("Don't let your struggle become your identity. You can do this! ");
+}
+
+function motivation() {
+   alert("You've got a new story to write, and it looks nothing like your past.");
+}
